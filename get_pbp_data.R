00e10404fdebdca6cd1ce88d12b1c5cbe66a5406
tri-ales <dtrigo2001@gmail.com>
2025-10-23 13:15:17 -0700

    Uploading main script

---
+++ get_pbp_data.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(bigballR)
+library(arrow)
+
+prev_day <- (Sys.Date())-1
+
+
+if (!dir.exists("data/2025-26"))
+  dir.create("data/2025-26")
+
+games <- get_date_games(date = prev_day)
+
+time <- c(5:8)
+
+pbp_dfs <- list()
+
+i <- 1
+while(i < nrow(games)) {
+  pbp <- get_play_by_play(games[i, 'GameID'])
+  pbp_dfs[[i]] <- pbp
+  
+  sleep <- sample(time)[1]
+  print(paste0("Resting for ", sleep, " seconds..." ))
+  Sys.sleep(sleep)  
+
+  i <- i + 1
+}
+
+pbp_all <- bind_rows(pbp_dfs)
+
+write_parquet(pbp_all, paste0("data/2025-26/",  "pbp_", gsub("-", "_", today), ".parquet"))
