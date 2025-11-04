b2cd7a15759c7323a7de6cafdf50b9d3d7ad2773
tri-ales <dtrigo2001@gmail.com>
2025-11-04 11:47:55 -0800

    Try and debug get_date_games fails

--- get_pbp_data.R
+++ get_pbp_data.R
@@ -9,6 +9,7 @@ if (!dir.exists("data/2025-26"))
   dir.create("data/2025-26")
 
 games <- get_date_games(date = prev_day)
+print(games)
 
 time <- c(5:8)
 
@@ -29,5 +30,6 @@ while(i <= nrow(games)) {
 pbp_all <- bind_rows(pbp_dfs)
 
 write_parquet(pbp_all, paste0("data/2025-26/",  "pbp_", gsub("-", "_", today), ".parquet"))
+
 
 
